e96257ebc14de99ae024a3b7a0115b24cffafa3c
rafianto <mulyonorafianto@gmail.com>
2022-11-15 11:08:34 +0700

    Ok

--- statistik_test1.py
+++ statistik_test1.py
@@ -2,7 +2,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, ax = plt.subplots()  # Create a figure containing a single axes.
+fig, ax = plt.subplots()  # Create a figure containing a single axes ok.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3]);  # Plot some data on the axes.pip
 plt.show()
 
