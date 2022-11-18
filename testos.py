4a7ed55c64e2858dc29e7e3cb8fb5a571b27ee21
rafianto <mulyonorafianto@gmail.com>
2022-11-18 09:04:24 +0700

    ok done

--- testos.py
+++ testos.py
@@ -4,7 +4,6 @@ db_user = os.environ.get('DB_USER')
 db_pass = os.environ.get('DB_PASS')
 db_pandas_exe_dir = os.environ.get('CONDA_PYTHON_EXE')
 
-
 print(db_user)
 print(db_pass)
 print(db_pandas_exe_dir)
