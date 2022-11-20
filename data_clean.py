f90476e173e23e9b48e59da97da1d3d34c72cfc0
rafianto <mulyonorafianto@gmail.com>
2022-11-21 05:32:13 +0700

    20221121-05.32

--- data_clean.py
+++ data_clean.py
@@ -22,13 +22,29 @@ try:
         port = port_id
     )
 
-    data = pd.read_sql('select * from public.crash',conn)
+    fdata = pd.read_sql('select * from public.crash',conn)
  
-    korelasi = data.corr();
+    cek1 = fdata.head()
+    # cek korelasi data
+    korelasi = fdata.corr();
     print(korelasi)
- 
+    print(fdata)
+    # cek korelasi 
     smp = sns.heatmap(korelasi)
-    plt.show()
+    sns.boxplot(x='number_of_drunk_drivers',data=korelasi)
+    #plt.show()
+ 
+    #menentukan q1,q3 batas bawah, batas atas dan selisih q3-q1 
+    q1,q3 = np.percentile(fdata['number_of_fatalities'], [25,75])
+    s1 = q3-q1
+    ba1 = q3+(1.5*s1) 
+    bw1 = q1-(1.5*s1)
+    print(q1)
+    print(q3)
+    print(s1)
+    print(ba1)
+    print(bw1)
+       
     
     conn.close()   
 except Exception as error:
